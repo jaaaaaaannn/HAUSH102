8301603928e8da3bff42b321809383faf2a526f2
Sam Gopez <sammgopez@gmail.com>
2017-01-23 20:10:36 +0800

    Activity 7

---
+++ activity7/scripts/myScript.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+
+	jQuery("#send-button").click(function() {
+		var inputMessage = jQuery("#message-input").val();
+
+		jQuery(".message-output-holder").append('<div class="message-holder"><img src="images/profile.png" class="profile-image"><div class="message-info"><p><strong>YOU</strong></p><p>' + inputMessage +'</p></div></div>');
+		jQuery("#message-input").val("");
+
+		$.ajax({
+			url:"http://sandbox.api.simsimi.com/request.p?key=2fe0b429-f279-44ec-8236-4d73c71971fb&lc=en&ft=1.0&text=" + inputMessage,
+			success: function(data) {
+				jQuery(".message-output-holder").append('<div class="simsimi-message-holder"><div class=""><p><strong>SIMSIMI</strong><br />' + data.response +'</p><img src="images/simsimi.png" class="simsimi-image"></div></div>');
+			}
+		});
+	});
+});
